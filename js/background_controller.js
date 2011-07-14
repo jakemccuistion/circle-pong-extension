0697d0e96a4e61a14d46a5ca13da3458a068bb18
Mohamed Mansour <hello@mohamedmansour.com>
2011-07-13 20:50:35 -0400

    Skeleton for Hangout Extension

---
+++ js/background_controller.js
@@ -0,0 +1,96 @@
+/**
+ * Manages a single instance of the entire application.
+ * @constructor
+ */
+BackgroundController = function()
+{
+  this.onExtensionLoaded();
+};
+
+/**
+ * Triggered when the extension just loaded. Should be the first thing
+ * that happens when chrome loads the extension.
+ */
+BackgroundController.prototype.onExtensionLoaded = function()
+{
+  var currVersion = chrome.app.getDetails().version;
+  var prevVersion = settings.version;
+  if (currVersion != prevVersion) {
+    // Check if we just installed this extension.
+    if (typeof prevVersion == 'undefined') {
+      this.onInstall();
+    } else {
+      this.onUpdate(prevVersion, currVersion);
+    }
+    settings.version = currVersion;
+  }
+};
+
+/**
+ * Triggered when the extension just installed.
+ */
+BackgroundController.prototype.onInstall = function()
+{
+  // Inject the content script to all opened window.
+  chrome.windows.getAll({ populate: true }, function(windows) {
+    for (var w = 0; w < windows.length; w++) {
+      var tabs = windows[w].tabs;
+      for (var t = 0; t < tabs.length; t++) {
+        var tab = tabs[t];
+        if (this.isValidURL(tab.url)) { 
+          chrome.tabs.executeScript(tab.id, { file: '/js/injection.js',
+                                    allFrames: true });
+        }
+      }
+    }
+  });
+};
+
+/**
+ * Check if the URL is a valid URL that we support for injection and processing.
+ *
+ * @param {string} url The URL to check.
+ */
+BackgroundController.prototype.isValidURL = function(url)
+{
+  return (url.indexOf('https://talkgadget.google.com') == 0 ||
+      url.indexOf('http://talkgadget.google.com') == 0);
+};
+
+/**
+ * Triggered when the extension just uploaded to a new version. DB Migrations
+ * notifications, etc should go here.
+ *
+ * @param {string} previous The previous version.
+ * @param {string} current  The new version updating to.
+ */
+BackgroundController.prototype.onUpdate = function(previous, current)
+{
+};
+
+/**
+ * Initialize the main Background Controller
+ */
+BackgroundController.prototype.init = function()
+{
+  chrome.extension.onRequest.addListener(this.onExtensionRequest.bind(this));
+};
+
+/**
+ * Listen for extension requests.
+ *
+ * @param {Object} request The request sent by the calling script.
+ * @param {Object<MessageSender>} sender The location where the script has spawned.
+ * @param {Function} request Function to call when you have a response. The 
+                              argument should be any JSON-ifiable object, or
+                              undefined if there is no response.
+ */
+BackgroundController.prototype.onExtensionRequest = function(request, sender, sendResponse) {
+  if (sender.tab && request.method == 'GetVersion') {
+    console.log('Content Script requesting data for GetSomeMethod');
+    sendResponse({data: settings.version});
+  }
+  else {
+    sendResponse({}); // snub
+  }
+};
