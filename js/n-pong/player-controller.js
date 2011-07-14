aa5cb7368a916595132f493679447259ad58e0d5
John Barrington Craggs <john.bc@gmail.com>
2011-07-13 19:21:07 -0700

    + circle pong basic game object templates....

---
+++ js/n-pong/player-controller.js
@@ -0,0 +1,34 @@
+/**
+ *  The Player User Controller
+ */
+PlayerUserController = function(){
+
+};
+
+PlayerUserController.prototype.init = function(gameState){
+	this.gameState= gameState;
+	this.goLeft = false;
+	this.goRight = false;
+
+};
+
+
+/*
+  - 
+*/
+PlayerUserController.prototype.onMoveEvent = function(dt) {
+	/* buffer move event */
+
+};
+
+
+/*
+  - 
+*/
+PlayerUserController.prototype.update = function(dt) {
+	/* take the buffered event left right
+	/* set goLeft or goRight
+};
+
+
+
