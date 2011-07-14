e90a76cf39485562737db7a660bfb0590eca0501
James Williams <james.l.williams@gmail.com>
2011-07-13 19:19:27 -0700

    Rewrote file to use JS inheritance/prototype and not Prototype.js

--- js/Game/Engine.js
+++ js/Game/Engine.js
@@ -1,31 +1,33 @@
 // Inspired by Seth Ladd's demo code
 
-var Engine = Class.create({
-	initialize: function() {
+function Engine() {
+	var self = this;
+	self.initialize = function() {
 		this.players = [];
 		this.collisionManager = null;
 		this.context = null;
 		this.lastUpdateTimestamp = null
 		this.deltaTime = null
 		this.ball = null
-	},
-	addPlayer: function (player) {
+	}
 	
-	},
-	
-	removePlayer: function (player) {
+	self.addPlayer = function(player) {
 		
-	},
+	}
 	
-	updateObjects: function() {
-	
-	},
-	
-	checkCollisions: function () {
+	self.removePlayer = function(player) {
 		
-	},
+	}
 	
-	loop: function() {
+	self.updateObjects = function() {
+		
+	}
+	
+	self.checkCollision = function() {
+		
+	}
+	
+	self.loop = function() {
 		var now = Date.now()
 		this.deltaTime = now - this.lastUpdateTimestamp;
 		this.checkCollisions();
@@ -34,21 +36,21 @@ var Engine = Class.create({
 		this.saveGameState();
 		this.transmitGameState();
 		this.lastUpdateTimestamp = now
-	},
+	}	
 	
-	updateObjects: function() {
+	self.updateObjects = function() {
 		// Server function
 		this.ball.update()
 		for (var i=0;i<this.players.length; i++) {
 			this.players[i].update();
 		}
-	},
+	}
 	
-	draw: function () {
+	self.draw = function () {
 		// Local function that draws on the client
-	},
+	}
 	
-	start: function () {
+	self.start = function () {
 		console.log("starting game with ____ as host")
 		this.lastUpdateTimestamp = Date.now()
 		var self = this
@@ -56,10 +58,11 @@ var Engine = Class.create({
 				self.loop();
 				requestAnimationFrame(gameLoop, self.ctx.canvas
 		})();
-	},
+	}
 	
-	saveGameState: function() {
+	self.saveGameState = function() {
 		// only host should run this function
 	}
 	
-});
+	self.initialize()
+}
