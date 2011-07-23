4ba884a08fbc13a0de705e246d3bbac8989a82ae
Mohamed Mansour <hello@mohamedmansour.com>
2011-07-23 01:11:18 -0400

    Parse out the chat messages for left and join group chats.
    This is to help eliminate the timer we are doing to see parts and
    joins.

--- js/background_controller.js
+++ js/background_controller.js
@@ -2,8 +2,8 @@
  * Manages a single instance of the entire application.
  * @constructor
  */
-BackgroundController = function()
-{
+BackgroundController = function() {
+  this.hangout_part_join_pattern = /^(.*) (joined|left) group chat\.$/;
   this.onExtensionLoaded();
 };
 
@@ -11,8 +11,7 @@ BackgroundController = function()
  * Triggered when the extension just loaded. Should be the first thing
  * that happens when chrome loads the extension.
  */
-BackgroundController.prototype.onExtensionLoaded = function()
-{
+BackgroundController.prototype.onExtensionLoaded = function() {
   var currVersion = chrome.app.getDetails().version;
   var prevVersion = settings.version;
   if (currVersion != prevVersion) {
@@ -29,8 +28,7 @@ BackgroundController.prototype.onExtensionLoaded = function()
 /**
  * Triggered when the extension just installed.
  */
-BackgroundController.prototype.onInstall = function()
-{
+BackgroundController.prototype.onInstall = function() {
   // Inject the content script to all opened window.
   chrome.windows.getAll({ populate: true }, function(windows) {
     for (var w = 0; w < windows.length; w++) {
@@ -59,15 +57,13 @@ BackgroundController.prototype.onInstall = function()
  * @param {string} previous The previous version.
  * @param {string} current  The new version updating to.
  */
-BackgroundController.prototype.onUpdate = function(previous, current)
-{
+BackgroundController.prototype.onUpdate = function(previous, current) {
 };
 
 /**
  * Initialize the main Background Controller
  */
-BackgroundController.prototype.init = function()
-{
+BackgroundController.prototype.init = function() {
   chrome.browserAction.onClicked.addListener(this.onBrowserActionClicked.bind(this));
   chrome.extension.onRequest.addListener(this.onExtensionRequest.bind(this));
 };
@@ -98,6 +94,37 @@ BackgroundController.prototype.onExtensionRequest = function(request, sender, se
  */
 BackgroundController.prototype.onChatReceived = function(data) {
   console.log('ChatReceived', data);
+  if (data.from == 'g') { // Google Event
+    var matchJoinPart = data.line.matches(this.hangout_part_join_pattern);
+    if (matchJoinPart) {
+      var name = matchJoinPart[1];
+      var state = matchJoinPart[2];
+      if (state == 'joined') {
+        this.onJoin(name);
+      }
+      else { // left
+        this.onPart(name);
+      }
+    }
+  }
+};
+
+/**
+ * Event when a user has joined the hangout.
+ *
+ * @param {string} name the player name.
+ */
+BackgroundController.prototype.onJoin = function(name) {
+  // Send to Game Context.
+};
+
+/**
+ * Event when a user has parted the hangout.
+ *
+ * @param {string} name the player name.
+ */
+BackgroundController.prototype.onPart = function(name) {
+  // Send to Game Context.
 };
 
 /**
@@ -114,7 +141,6 @@ BackgroundController.prototype.onParticipantsReceived = function(data) {
  *
  * @param {Object} tab The current tab that the browser action is referring to.
  */
-BackgroundController.prototype.onBrowserActionClicked = function(tab)
-{
+BackgroundController.prototype.onBrowserActionClicked = function(tab) {
   chrome.windows.create({url: chrome.extension.getURL('game.html'), width: 500, height: 500, type: 'popup' });
 };
